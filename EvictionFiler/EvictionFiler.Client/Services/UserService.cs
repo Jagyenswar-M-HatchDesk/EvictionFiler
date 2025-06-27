31674f9920237143a252ad9503c324baf1d2824c
Jagyenswar <meher.jagy2002@gmail.com>
2025-06-27 18:54:36 +0530

    add user initial push

---
+++ EvictionFiler/EvictionFiler.Client/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using EvictionFiler.Application.DTOs;
+using EvictionFiler.Application.Interfaces.IServices;
+using EvictionFiler.Domain.Entities;
+
+namespace EvictionFiler.Client.Services
+{
+    public class UserServiceClient
+    {
+        private readonly IUserservices _userService;
+        
+
+        public UserServiceClient(IUserservices userService)
+        {
+            _userService = userService;
+           
+        }
+
+        
+
+       
+
+        public async Task<bool> RegisterTenantAsync(RegisterDto model)
+        {
+            var result = await _userService.RegisterTenantAsync(model);
+            if (result == false) return false;
+            return true;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUser()
+        {
+            var result = await _userService.GetAllUserAsync();
+            return result;
+        }
+
+    }
+}
--- EvictionFiler/EvictionFiler/Program.cs
+++ EvictionFiler/EvictionFiler/Program.cs
@@ -1,4 +1,8 @@
 using EvictionFiler.Client.Pages;
+using EvictionFiler.Client.Services;
+using EvictionFiler.Domain.Entities;
+using EvictionFiler.Infrastructure.DbContexts;
+using EvictionFiler.Infrastructure.Repositories;
 using EvictionFiler.Server.Components;
 using Syncfusion.Blazor;
 
@@ -12,6 +16,22 @@ builder.Services.AddServerSideBlazor()
     .AddCircuitOptions(options => { options.DetailedErrors = true; });
 
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddDbContext<MainDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"),
+        b => b.MigrationsAssembly("EvictionFiler.Infrastructure")));
+
+
+builder.Services.AddScoped<IUserservices, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<JwtAuthStateProvider>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<JwtAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+    provider.GetRequiredService<JwtAuthStateProvider>());
+builder.Services.AddScoped<UserServiceClient>();
+builder.Services.AddAuthorizationCore();
 
 var app = builder.Build();
 
