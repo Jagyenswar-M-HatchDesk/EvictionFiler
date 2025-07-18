9499b9939eb362161dd97724316ef8b4fb427c76
Ankit Rajput <kumarankit90653@gmail.com>
2025-07-18 09:18:13 +0530

    changes

--- EvictionFiler.Application/DTOs/CreateEditLegalCaseModel.cs
+++ EvictionFiler.Application/DTOs/CreateEditLegalCaseModel.cs
@@ -1,55 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EvictionFiler.Application.DTOs
 {
-    public class CreateEditLegalCaseModel
-    {
-        public Guid Id { get; set; }
-        public Guid? ClientId { get; set; }
-        public Guid? ApartmentId { get; set; }
-        public Guid? LandLordId { get; set; }
-        public Guid? TenantId { get; set; }
-        public string? CaseName { get; set; }
-        public string? ClientRole { get; set; }
-        public string? LegalRepresentative { get; set; }
-        public string? Casecode { get; set; }
+	public class CreateEditLegalCaseModel : IValidatableObject
+	{
+		public Guid Id { get; set; }
+		public Guid? ClientId { get; set; }
+		public Guid? ApartmentId { get; set; }
+		public Guid? LandLordId { get; set; }
+		public Guid? TenantId { get; set; }
+		public string? CaseName { get; set; }
+		public string? ClientRole { get; set; }
+		public string? LegalRepresentative { get; set; }
+		public string? Casecode { get; set; }
 
+		//section4
+		public string? Company { get; set; }
+		public string? Contact { get; set; }
+		public string? PhoneorEmail { get; set; }
+		public string? MDRNo { get; set; }
+		public string? ResidentalUnits { get; set; }
+		public string? CommercialUnits { get; set; }
+		public bool? AllUnitsRehistered { get; set; }
+		public bool? HPDViolation { get; set; }
 
-        //section4
-        public string? Company { get; set; }
-        public string? Contact { get; set; }
-        public string? PhoneorEmail { get; set; }
-        public string? MDRNo { get; set; }
-        public string? ResidentalUnits { get; set; }
-        public string? CommercialUnits { get; set; }
-        public bool? AllUnitsRehistered { get; set; }
-        public bool? HPDViolation { get; set; }
+		//section5
+		public string? Address { get; set; }
+		public string? AptNo { get; set; }
+		public string? Borough { get; set; }
+		public string? ZIP { get; set; }
+		public string? Class { get; set; }
+		public string? YearBuilt { get; set; }
+		public bool? HeatorHotWater { get; set; }
+		public bool? RentStablized { get; set; }
 
-        //section5
-        public string? Address { get; set; }
-        public string? AptNo { get; set; }
-        public string? Borough { get; set; }
-        public string? ZIP { get; set; }
-        public string? Class { get; set; }
-        public string? YearBuilt { get; set; }
-        public bool? HeatorHotWater { get; set; }
-        public bool? RentStablized { get; set; }
+		//section6
+		public bool? SeekinEviction { get; set; }
+		public bool? ExemptUnit { get; set; }
+		public bool? RentIncreases { get; set; }
+		public int? Monthsunpaid { get; set; }
+		public string? OthersGrounds { get; set; }
 
-        //section6
-
-        public bool? SeekinEviction { get; set; }
-        public bool? ExemptUnit { get; set; }
-        public bool? RentIncreases { get; set; }
-        public int? Monthsunpaid { get; set; }
-        public string? OthersGrounds { get; set; }
-		[Required(ErrorMessage = "CaseType  is required")]
+		[Required(ErrorMessage = "Case Type is required")]
 		public Guid? CaseTypeId { get; set; }
-		[Required(ErrorMessage = "CasesUBType  is required")]
+
+		// Removed Required from here
 		public Guid? CaseSubTypeId { get; set; }
 
 		[Required(ErrorMessage = "Attorney is required")]
@@ -60,5 +57,17 @@ namespace EvictionFiler.Application.DTOs
 
 		[Required(ErrorMessage = "Firm is required")]
 		public string Firm { get; set; }
+
+		// 👇 Add this property to receive selected case type name from the component
+		public string SelectedCaseTypeName { get; set; }
+
+		// ✅ IValidatableObject method
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (SelectedCaseTypeName == "Holdover" && CaseSubTypeId == null)
+			{
+				yield return new ValidationResult("Case Sub Type is required for Holdover case type.", new[] { nameof(CaseSubTypeId) });
+			}
+		}
 	}
 }
