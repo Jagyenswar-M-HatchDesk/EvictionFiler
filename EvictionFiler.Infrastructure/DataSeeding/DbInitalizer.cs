59193a4ece488d5267cdb3a6adec35fe08d66260
Ankit Rajput <kumarankit90653@gmail.com>
2025-07-21 10:30:51 +0530

    changes

---
+++ EvictionFiler.Infrastructure/DataSeeding/DbInitalizer.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using EvictionFiler.Infrastructure.DbContexts;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace EvictionFiler.Infrastructure.DataSeeding
+//{
+//	public class DbInitalizer
+//	{
+//		public static async Task Seed(MainDbContext context)
+//		{
+//			try
+//			{
+//				await context.Database.MigrateAsync();
+//				await SeedCaseTypes(context);
+		
+//				await context.SaveChangesAsync();
+//			}
+//			catch (Exception ex)
+//			{
+//				throw;
+//			}
+//		}
+
+//		private static async Task SeedCaseTypes(MainDbContext context)
+//		{
+//			var casetypes = InitialDataGenerator.GetCaseTypes();
+//			foreach (var type in casetypes)
+//			{
+//				if (context.CaseTypes.FirstOrDefault(d => d.Name == type.Name) == null)
+//				{
+//					await context.CaseTypes.AddAsync(casetypes);
+//				}
+//			}
+//		}
+
+//		private static async Task SeedCaseSubTypes(MainDbContext context)
+//		{
+//			var subTypes = InitialDataGenerator.GetCaseSubTypes();
+//			foreach (var subtype in subTypes)
+//			{
+//				if (context.CaseSubTypes.FirstOrDefault(d => d.Name == subtype.Name) == null)
+//				{
+//					await context.CaseTypes.AddAsync(subTypes);
+//				}
+//			}
+//		}
+//	}
+//}
---
+++ EvictionFiler.Infrastructure/DataSeeding/InitialDataGenerator.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+//using EvictionFiler.Domain.Entities;
+
+//namespace EvictionFiler.Infrastructure.DataSeeding
+//{
+//	public class InitialDataGenerator
+//	{
+//		public static IEnumerable<CaseType> GetCaseTypes()
+//		{
+
+//			return new List<CaseType>
+//			{
+//				new CaseType() { Name = "Holdover"},
+//				new CaseType() { Name = "HP Action"},
+//				new CaseType() { Name = "NonPayment"},
+//			    new CaseType() { Name = "NonPayment"},
+//			};
+//		}
+
+//	}
+//}
