616d24c730500de7860ec7c837e98ef5eb5dcf86
Ankit Rajput <kumarankit90653@gmail.com>
2025-08-13 18:50:21 +0530

    completed changes

--- EvictionFiler.Infrastructure/Repositories/CaseFormRepository.cs
+++ EvictionFiler.Infrastructure/Repositories/CaseFormRepository.cs
@@ -26,164 +26,114 @@ namespace EvictionFiler.Infrastructure.Repositories
 		_context = context;
 	}
 
-		//public async Task<bool> GenerateNoticeAsync(Guid legalCaseId, Guid formTypeId)
-		//{
-		//	// 1. Template fetch
-		//	var template = await _context.MstFormTypes
-		//		.Where(f => f.Id == formTypeId)
-		//		.Select(f => f.HTML)
-		//		.FirstOrDefaultAsync();
-
-		//	if (string.IsNullOrEmpty(template))
-		//		throw new Exception("Template not found.");
-
-		//	// 2. Case + landlord + property details
-		//	var caseDetails = await (from lc in _context.LegalCases
-		//							 join landlord in _context.LandLords on lc.LandLordId equals landlord.Id
-		//							 join building in _context.Buildings on lc.BuildingId equals building.Id
-		//							 join tenant in _context.Tenants on lc.TenantId equals tenant.Id
-		//							 where lc.Id == legalCaseId
-		//							 select new
-		//							 {
-		//								 LandlordName = landlord.FirstName + " " + landlord.LastName,
-		//								 LandlordAddress = landlord.Address1 + " " + landlord.Address2 + " " +
-		//												   landlord.City + " " + landlord.State.Name + " " + landlord.Zipcode,
-		//								 LandlordPhone = landlord.Phone,
-		//								 LandlordDate  = landlord.CreatedOn,
-		//								 LandlordEmail = landlord.Email,
-		//								 PropertyAddress = building.Address1 + " " + building.Address2 + " " +
-		//												   building.City + " " + building.State.Name + " " + building.Zipcode,
-		//								 NumberofRoom = building.BuildingUnits.ToString(),
-		//								 ApartmentNumber = tenant.UnitOrApartmentNumber,
-		//								 TenantName = tenant.FirstName + "" + tenant.LastName
-		//							 }).FirstOrDefaultAsync();
-
-		//	if (caseDetails == null)
-		//		throw new Exception("Case details not found.");
-
-		//	string filledHtml = template
-		//		.Replace("{{LandlordName}}", caseDetails.LandlordName ?? "")
-		//		.Replace("{{LandlordAddress}}", caseDetails.LandlordAddress ?? "")
-		//		.Replace("{{LandlordPhone}}", caseDetails.LandlordPhone ?? "")
-		//		.Replace("{{LandlordEmail}}", caseDetails.LandlordEmail ?? "")
-		//		.Replace("{{LandlordDate}}", caseDetails.LandlordDate.ToString("MM/dd/yyyy"))
-		//		.Replace("{{PropertyAddress}}", caseDetails.PropertyAddress ?? "")
-		//		.Replace("{{ApartmentNumber}}", caseDetails.ApartmentNumber ?? "")
-		//		.Replace("{{NumberofRoom}}", caseDetails.NumberofRoom ?? "")
-		//	.Replace("{{TenantName}}", caseDetails.TenantName ?? "");
-
-		//	var caseForm = new CaseForms
-		//	{
-		//		Id = Guid.NewGuid(),
-		//		LegalCaseId = legalCaseId,
-		//		FormTypeId = formTypeId,
-		//		HTML = filledHtml,
-		//		File = null,
-		//		CreatedOn = DateTime.UtcNow,
-		//		IsDeleted = false
-		//	};
-
-		//	_context.CaseForms.Add(caseForm);
-		//	return await _context.SaveChangesAsync() > 0;
-		//}
 
 
 		public async Task<bool> GenerateNoticeAsync(Guid legalCaseId, Guid formTypeId)
 		{
-			// 1. Template fetch
-			var template = await _context.MstFormTypes
-				.Where(f => f.Id == formTypeId)
-				  .Select(f => new { f.HTML, f.Name })
-				.FirstOrDefaultAsync();
-
-			if (string.IsNullOrEmpty(template.HTML))
-				throw new Exception("Template not found.");
-
-			DateTime noticeDate = CalculateNoticeDate(template.Name);
-
-			// 2. Case + landlord + property details
-			var caseDetails = await (from lc in _context.LegalCases
-									 join landlord in _context.LandLords on lc.LandLordId equals landlord.Id
-									 join building in _context.Buildings on lc.BuildingId equals building.Id
-									 join tenant in _context.Tenants on lc.TenantId equals tenant.Id
-									 where lc.Id == legalCaseId
-									 select new
-									 {
-										 LandlordName = landlord.FirstName + " " + landlord.LastName,
-										 LandlordAddress = landlord.Address1 + " " + landlord.Address2 + " " +
-														   landlord.City + " " + landlord.State.Name + " " + landlord.Zipcode,
-										 LandlordPhone = landlord.Phone,
-										 
-										 LandlordEmail = landlord.Email,
-										 PropertyAddress = building.Address1 + " " + building.Address2 + " " +
-														   building.City + " " + building.State.Name + " " + building.Zipcode,
-										 NumberofRoom = building.BuildingUnits.ToString(),
-										 ApartmentNumber = tenant.UnitOrApartmentNumber,
-										 TenantName = tenant.FirstName + " " + tenant.LastName
-									 }).FirstOrDefaultAsync();
-
-			if (caseDetails == null)
-				throw new Exception("Case details not found.");
-
-			// 3. Fill HTML
-			string filledHtml = template.HTML
-				.Replace("{{LandlordName}}", caseDetails.LandlordName ?? "")
-				.Replace("{{LandlordAddress}}", caseDetails.LandlordAddress ?? "")
-				.Replace("{{LandlordPhone}}", caseDetails.LandlordPhone ?? "")
-				.Replace("{{LandlordEmail}}", caseDetails.LandlordEmail ?? "")
-				.Replace("{{LandlordDate}}", noticeDate.ToString("MM/dd/yyyy"))
-				.Replace("{{PropertyAddress}}", caseDetails.PropertyAddress ?? "")
-				.Replace("{{ApartmentNumber}}", caseDetails.ApartmentNumber ?? "")
-				.Replace("{{CurrentDate}}", DateTime.Now.ToString("MM/dd/yyyy"))
-				.Replace("{{NumberofRoom}}", caseDetails.NumberofRoom ?? "")
-				.Replace("{{TenantName}}", caseDetails.TenantName ?? "");
-
-			// 4. Generate PDF from HTML using PuppeteerSharp
-			await new BrowserFetcher().DownloadAsync();
-			await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+			try
 			{
-				Headless = true,
-				Args = new[] { "--no-sandbox" }
-			});
+				
+				var template = await _context.MstFormTypes
+					.Where(f => f.Id == formTypeId)
+					.Select(f => new { f.HTML, f.Name })
+					.FirstOrDefaultAsync();
 
-			var page = await browser.NewPageAsync();
-			await page.SetContentAsync(filledHtml, new NavigationOptions
+				if (string.IsNullOrEmpty(template?.HTML))
+					throw new Exception("Template not found.");
+
+				DateTime noticeDate = CalculateNoticeDate(template.Name);
+
+		
+				var caseDetails = await (from lc in _context.LegalCases
+										 join landlord in _context.LandLords on lc.LandLordId equals landlord.Id
+										 join building in _context.Buildings on lc.BuildingId equals building.Id
+										 join tenant in _context.Tenants on lc.TenantId equals tenant.Id
+										 where lc.Id == legalCaseId
+										 select new
+										 {
+											 CaseCode = lc.Casecode,
+											 LandlordName = landlord.FirstName + " " + landlord.LastName,
+											 LandlordAddress = landlord.Address1 + " " + landlord.Address2 + " " +
+															   landlord.City + " " + landlord.State.Name + " " + landlord.Zipcode,
+											 LandlordPhone = landlord.Phone,
+											 LandlordEmail = landlord.Email,
+											 PropertyAddress = building.Address1 + " " + building.Address2 + " " +
+															   building.City + " " + building.State.Name + " " + building.Zipcode,
+											 NumberofRoom = building.BuildingUnits.ToString(),
+											 ApartmentNumber = tenant.UnitOrApartmentNumber,
+											 TenantName = tenant.FirstName + " " + tenant.LastName
+										 }).FirstOrDefaultAsync();
+
+				if (caseDetails == null)
+					throw new Exception("Case details not found.");
+
+				// 3. Fill HTML
+				string filledHtml = template.HTML
+					.Replace("{{LandlordName}}", caseDetails.LandlordName ?? "")
+					.Replace("{{LandlordAddress}}", caseDetails.LandlordAddress ?? "")
+					.Replace("{{LandlordPhone}}", caseDetails.LandlordPhone ?? "")
+					.Replace("{{LandlordEmail}}", caseDetails.LandlordEmail ?? "")
+					.Replace("{{LandlordDate}}", noticeDate.ToString("MM/dd/yyyy"))
+					.Replace("{{PropertyAddress}}", caseDetails.PropertyAddress ?? "")
+					.Replace("{{ApartmentNumber}}", caseDetails.ApartmentNumber ?? "")
+					.Replace("{{CurrentDate}}", DateTime.Now.ToString("MM/dd/yyyy"))
+					.Replace("{{NumberofRoom}}", caseDetails.NumberofRoom ?? "")
+					.Replace("{{TenantName}}", caseDetails.TenantName ?? "");
+
+			
+				await new BrowserFetcher().DownloadAsync();
+				await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+				{
+					Headless = true,
+					Args = new[] { "--no-sandbox" }
+				});
+
+				var page = await browser.NewPageAsync();
+				await page.SetContentAsync(filledHtml, new NavigationOptions
+				{
+					WaitUntil = new[] { WaitUntilNavigation.Networkidle0 }
+				});
+
+				var pdfBytes = await page.PdfDataAsync(new PdfOptions
+				{
+					Format = PaperFormat.A4,
+					PrintBackground = true
+				});
+
+				
+				var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "CaseForms", caseDetails.CaseCode);
+				Directory.CreateDirectory(folderPath);
+
+				var fileName = $"{Guid.NewGuid()}.pdf";
+				var filePath = Path.Combine(folderPath, fileName);
+				await File.WriteAllBytesAsync(filePath, pdfBytes);
+
+				
+				var fileUrl = $"/CaseForms/{caseDetails.CaseCode}/{fileName}";
+
+			
+				var caseForm = new CaseForms
+				{
+					Id = Guid.NewGuid(),
+					LegalCaseId = legalCaseId,
+					FormTypeId = formTypeId,
+					HTML = filledHtml,
+					File = fileUrl,
+					CreatedOn = DateTime.UtcNow,
+					IsDeleted = false
+				};
+
+				_context.CaseForms.Add(caseForm);
+				return await _context.SaveChangesAsync() > 0;
+			}
+			catch (Exception ex)
 			{
-				WaitUntil = new[] { WaitUntilNavigation.Networkidle0 }
-			});
-
-			var pdfBytes = await page.PdfDataAsync(new PdfOptions
-			{
-				Format = PaperFormat.A4,
-				PrintBackground = true
-			});
-
-			// 5. Save PDF to wwwroot folder
-			var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "CaseForms");
-			Directory.CreateDirectory(folderPath);
-
-			var fileName = $"{Guid.NewGuid()}.pdf";
-			var filePath = Path.Combine(folderPath, fileName);
-			await File.WriteAllBytesAsync(filePath, pdfBytes);
-
-			// Public URL
-			var fileUrl = $"/CaseForms/{fileName}";
-
-			// 6. Save to CaseForms
-			var caseForm = new CaseForms
-			{
-				Id = Guid.NewGuid(),
-				LegalCaseId = legalCaseId,
-				FormTypeId = formTypeId,
-				HTML = filledHtml,
-				File = fileUrl, 
-				CreatedOn = DateTime.UtcNow,
-				IsDeleted = false
-			};
-
-			_context.CaseForms.Add(caseForm);
-			return await _context.SaveChangesAsync() > 0;
+				
+				Console.WriteLine($"Error generating notice: {ex.Message}");
+				return false;
+			}
 		}
+
 
 		private DateTime CalculateNoticeDate(string formName)
 		{
