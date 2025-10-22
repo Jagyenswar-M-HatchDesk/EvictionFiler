6c4c9022fdf42ac2cf9e9bdc49b50fdbb0340330
Jagyenswar <meher.jagy2002@gmail.com>
2025-10-22 18:25:14 +0530

    calender mistakes

---
+++ EvictionFiler.Domain/Entities/LegalcaseCourtInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvictionFiler.Domain.Entities
+{
+    public class LegalcaseCourtInfo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Court { get; set; }
+        public string? RoomNo { get; set; }
+        public string? Part { get; set; }
+        public string? Judge { get; set; }
+        public string? Marshal { get; set; }
+        public string? DocketNo { get; set; }
+        public string? IndexNo { get; set; }
+    }
+}
