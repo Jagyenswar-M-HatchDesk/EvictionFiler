e8c3fbf173b865d49310d79a80d26a5685bc90fe
vipin k <VShukla@refugees.org>
2025-10-27 18:30:14 +0530

    create hpd and perdiem html design

--- EvictionFiler.Domain/Enums/Step.cs
+++ EvictionFiler.Domain/Enums/Step.cs
@@ -15,7 +15,39 @@ namespace EvictionFiler.Domian.Enums
         Terms = 4,
         Rent = 6,
         Case = 5,
-        Notice =7,
-       
+        Notice = 7,
+
+    }
+
+    public enum StepHpd
+    {
+        CaseAndclient = 0,
+        Attorney = 0,
+        Case = 1,
+        Party = 2,
+        Building = 3,
+        PetitionerTenant = 4,
+        RespondentLandlord = 5,
+        Appearance = 6,
+        Relief = 7,
+        Billing = 8,
+
+    }
+
+    public enum StepPerdiem
+    {
+        Attorney = 0,
+        Case = 1,
+        Party = 2,
+        Appearance = 3,
+        CaseBackground=4,
+        DocumentIntractions = 5,
+        Reporting = 6, 
+        Billing = 7,
+        Acknowledgment=8,
+
+
+
+
     }
 }
