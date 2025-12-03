bea8802bbb5f503cef1bbc7824bd03fc06a6a245
Priyanshu Rajput <priynashuu.r.hatchdesk@gmail.com>
2025-12-03 18:56:21 +0530

    Marshal Crud Operation

---
+++ EvictionFiler.Application/DTOs/MarshalsDto/MarshalDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvictionFiler.Application.DTOs.MarshalsDto
+{
+    public class MarshalDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+        public int BadgeNumber { get; set; }
+
+        public string Telephone { get; set; } = string.Empty;
+        public string Fax { get; set; } = string.Empty;
+
+        public string OfficeAddress { get; set; } = string.Empty;
+    }
+}
---
+++ EvictionFiler.Application/Interfaces/IRepository/IMarshalRepositroy.cs
@@ -0,0 +1,15 @@
+﻿//using EvictionFiler.Domain.Entities;
+using Marshal = EvictionFiler.Domain.Entities.Marshal;
+
+namespace EvictionFiler.Application.Interfaces.IRepository
+{
+    public interface IMarshalRepositroy
+    {
+        Task<Marshal> GetMarshalByIdAsync(Guid id);
+        Task<IEnumerable<Marshal>> GetAllMarshalAsync();
+
+        Task<Marshal> AddMarshalAsync(Marshal marshal);
+        Task<Marshal> UpdateMarshalAsync(Marshal marshal);
+        Task<bool> DeleteMarshalAsync(Guid id);
+    }
+}
