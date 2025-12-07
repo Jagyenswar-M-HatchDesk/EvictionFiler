54878d9eb9406ef986ba216ecb62b1508c59b2f8
Sachin Tanwar <isachin100@gmail.com>
2025-12-07 17:37:12 +0530

    migrations corrections.

---
+++ EvictionFiler.Infrastructure/Migrations/20251207120418_pending-changes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvictionFiler.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class pendingchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_LegalCases_NoticeId",
+            //    table: "LegalCases",
+            //    column: "NoticeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LegalCases_NoticeId",
+                table: "LegalCases");
+        }
+    }
+}
