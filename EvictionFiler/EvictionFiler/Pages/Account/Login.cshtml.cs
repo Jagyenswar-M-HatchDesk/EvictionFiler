4c8f9ef263544dece0aceea9bd41dc095763e8b1
Sachin Tanwar <isachin100@gmail.com>
2026-02-04 20:17:45 +0530

    Basic corrections - client validations - rate limiting - speedup the page - anti forgery token

--- EvictionFiler/EvictionFiler/Pages/Account/Login.cshtml.cs
+++ EvictionFiler/EvictionFiler/Pages/Account/Login.cshtml.cs
@@ -1,14 +1,14 @@
 using EvictionFiler.Application.DTOs.UserDto;
-//using EvictionFiler.Client.Services;
 using EvictionFiler.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace EvictionFiler.Server.Components.Pages.Account
 {
+    [EnableRateLimiting("login")]
     public class LoginModel : PageModel
     {
         private readonly SignInManager<User> _signInManager;
