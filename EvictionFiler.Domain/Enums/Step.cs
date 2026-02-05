db9c14a844f0c32d6eb22c2455b2267815ed4775
Jagyenswar <meher.jagy2002@gmail.com>
2026-02-05 14:43:30 +0530

    create case validation issue resolved

--- EvictionFiler.Domain/Enums/Step.cs
+++ EvictionFiler.Domain/Enums/Step.cs
@@ -14,9 +14,7 @@ namespace EvictionFiler.Domian.Enums
         Building = 2,
         Tenant = 3,
         Terms = 4,
-        //Case = 6,
-        //Rent = 5,
-        //Notice = 8,
+        
         Review = 6,
 
 
@@ -28,17 +26,14 @@ namespace EvictionFiler.Domian.Enums
         Landlord = 1,
         Building = 2,
         Tenant = 3,
-        //Arrears = 5,
-        //Notice = 5,
-        //GoodCause = 6,
-        //Filing = 8,
+        
         Review = 4,
     }
 
     public enum StepHPD
     {
         CaseAndclient = 0,
-        //Attorney = 1,
+        
         CaseInfo = 1,
         Building = 2,
         Party = 3,
@@ -50,37 +45,35 @@ namespace EvictionFiler.Domian.Enums
         Review = 9,
 
     }
-
-    public enum StepIllegalLockout
-    {
-        CaseAndclient = 0,
-        //Attorney = 1,
-        CaseInfo = 1,
-        Building = 2,
-        Party = 3,
-        Petitioner = 4,
-        Respondent = 5,
-        Appearance = 6,
-        Relief = 7,
-        Billing = 8,
-        Review = 9,
-
-    }
-
     public enum StepPerDiem
     {
         CaseAndclient = 0,
-        //Attorney = 1,
+
         CaseInfo = 1,
-        //Party = 2,
-        //Appearance = 3,
+
         Background = 2,
         Document = 3,
         Reporting = 4,
-        //Billing = 5,
-        //Acknowledgment = 9,
+
 
     }
+    public enum StepIllegalLockout
+    {
+        CaseAndclient = 0,
+       
+        CaseInfo = 1,
+        Building = 2,
+        Party = 3,
+        Petitioner = 4,
+        Respondent = 5,
+        Appearance = 6,
+        Relief = 7,
+        Billing = 8,
+        Review = 9,
+
+    }
+
+   
     public enum StepTenants
     {
         CaseAndclient = 0,
